68a72f35a32d4cee5be556d7501014ed76fae526
spiritevolution <158512066+spiritevolution@users.noreply.github.com>
2024-05-13 13:46:12 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,13 @@
+}
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+        int[] data = new int[10];
+
+
+    }
+        for (int item : data) {
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
+        }
